70501c22d172d30f936842f65821ef2bb8702df2
Greg Walker <michael.walker@gsa.gov>
2016-09-22 17:45:58 -0500

    Add and configure Devise. Make the User model a plain object rather than a database object.  Users come from outside and don't need to be persisted anywhere by the API.

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User
+  include ActiveModel::Validations #required because some before_validations are defined in devise
+  extend ActiveModel::Callbacks #required to define callbacks
+  extend Devise::Models
+
+  define_model_callbacks :validation #required by Devise
+
+  def initialize(jwt)
+    @role = jwt['role']
+  end
+
+  def role
+    @role
+  end
+
+  def self.serialize_into_session(o)
+    ''
+  end
+end
