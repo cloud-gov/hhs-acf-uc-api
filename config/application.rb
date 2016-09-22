045c9ba81dc42ce0f7517e5e1ad9370191e0b8b3
Greg Walker <michael.walker@gsa.gov>
2016-09-22 17:45:58 -0500

    Add and configure Devise. Make the User model a plain object rather than a database object.  Users come from outside and don't need to be persisted anywhere by the API.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module HhsAcfUcApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Flash
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   resources :enrollments do
     get 'count', on: :collection
   end
