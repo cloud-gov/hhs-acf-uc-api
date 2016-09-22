736741c5dab221553822deef2addbd84abf996f0
Catherine Devlin <catherine.devlin@gsa.gov>
2016-09-22 14:06:26 -0400

    in_care and on parameters working

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class EnrollmentsController < ApplicationController
   before_action :set_enrollment, only: [:show, :update, :destroy]
 
@@ -12,19 +14,31 @@ class EnrollmentsController < ApplicationController
     render json: {"enrollments": @enrollments.count}
   end
 
-  REJECTS = ['action', 'controller']
+  NOFILTER = ['on', 'action', 'controller']
+  IN_CARE = "uac_id %s IN
+    ( SELECT uac_id
+      FROM   uac_program_info
+      WHERE  date_referred < ?
+      AND (date_discharged IS NULL
+           OR date_discharged > ?)
+    )"
 
   def query
     @enrollments = Enrollment.all
 
-    binding.pry
-
-    params.reject {|k, v| REJECTS.include? k}.each do |k, v|
-      @enrollments = @enrollments.where(k => v)
+    params.reject {|k, v| NOFILTER.include? k}.each do |k, v|
+      case k.downcase
+      when "in_care", "!in_care"
+        qry = IN_CARE % sql_not(k, v)
+        @enrollments = @enrollments.where(qry, on, on)
+      else
+        @enrollments = @enrollments.where(k => v)
+      end
       # @enrollments = @enrollments.where("#{k} = ?", v)
       # TODO: Support >, <, >=, <= by detecting them
       # in the key or value
     end
+
   end
 
   # So would `/placements?on=2016-03-12` give the same result that `/enrollments?date_orr_approved=2016-03-12` does?
@@ -35,9 +49,38 @@ class EnrollmentsController < ApplicationController
   end
 
   private
+
+    def on
+      @on ||= begin
+        if params.include? 'on' then
+          on = Date.parse params['on']
+        else
+          on = Date.today
+        end
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_enrollment
       @enrollment = Enrollment.find(params[:id])
+    end
+
+    def cleaned(val)
+      begin
+        return val.downcase.strip
+      rescue NoMethodError
+        return val
+      end
+    end
+
+    FALSEY = ["f", "n", "no", "false", "0"]
+
+    def sql_not(k, v)
+      result = ! (k.starts_with? "!")
+      if FALSEY.include? cleaned(v)
+        result = !result
+      end
+      result ? "" : "NOT"
     end
 
     # Only allow a trusted parameter "white list" through.
