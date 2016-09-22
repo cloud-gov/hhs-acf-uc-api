51f25b2c7a6f538d6338e4d7caca8b1bb0929409
Greg Walker <michael.walker@gsa.gov>
2016-09-22 18:01:44 -0500

    Add JWT strategy, and add it to Devise; keep keys out of git

---
+++ config/initializers/jwt_strategy.rb
@@ -0,0 +1,33 @@
+require 'JWT'
+require 'openssl'
+
+module Devise
+  module Strategies
+    class JWTStrategy < Base
+      def valid?
+        !request.headers['Authorization'].nil?
+      end
+
+      def authenticate!
+        if claims
+          success!(User.new(claims))
+        else
+          fail!('Could not get claim')
+        end
+      end
+
+      private
+
+      def claims
+        if File.exist?('keys/public.key')
+          public_key = OpenSSL::PKey::RSA.new(File.read('keys/public.key'))
+          auth_header = request.headers['Authorization'] and
+            token = auth_header.split(' ').last and
+              ::JWT.decode(token, public_key, true, { :algorithm => 'RS512' })[0]
+        else
+          nil
+        end
+      end
+    end
+  end
+end
