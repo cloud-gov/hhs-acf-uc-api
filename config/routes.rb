9943d22310e5820ad88e52765c7d89810302a136
Catherine Devlin <catherine.devlin@gsa.gov>
2016-09-22 09:57:31 -0400

    Add /enrollments/count

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :enrollments
+  resources :enrollments do
+    get 'count', on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
