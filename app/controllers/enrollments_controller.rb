48a0ebc8a5570ac7644167a043bbf2f64afcb25f
Catherine Devlin <catherine.devlin@gsa.gov>
2016-09-22 09:57:31 -0400

    Add /enrollments/count

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,10 +3,31 @@ class EnrollmentsController < ApplicationController
 
   # GET /enrollments
   def index
-    @enrollments = Enrollment.all
-
+    self.query
     render json: @enrollments
   end
+
+  def count
+    self.query
+    render json: {"enrollments": @enrollments.count}
+  end
+
+  REJECTS = ['action', 'controller']
+
+  def query
+    @enrollments = Enrollment.all
+
+    binding.pry
+
+    params.reject {|k, v| REJECTS.include? k}.each do |k, v|
+      @enrollments = @enrollments.where(k => v)
+      # @enrollments = @enrollments.where("#{k} = ?", v)
+      # TODO: Support >, <, >=, <= by detecting them
+      # in the key or value
+    end
+  end
+
+  # So would `/placements?on=2016-03-12` give the same result that `/enrollments?date_orr_approved=2016-03-12` does?
 
   # GET /enrollments/1
   def show
