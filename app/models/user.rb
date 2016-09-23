89731e294d9d8927bbe2dfd4b69acc6ae83becdb
Greg Walker <michael.walker@gsa.gov>
2016-09-23 07:44:43 -0500

    Return an empty array on authentication_keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,19 @@ class User
     @role
   end
 
+  # There's no session, so don't bother with serialization.
   def self.serialize_into_session(o)
     ''
   end
 
+  # Disable HTTP Basic Auth for this class.
   def self.http_authenticatable
     false
+  end
+
+  # ¯\_(ツ)_/¯ - but if this isn't here and the Authorization
+  # header isn't set, we get a 500 server error.
+  def self.authentication_keys
+    [ ]
   end
 end
